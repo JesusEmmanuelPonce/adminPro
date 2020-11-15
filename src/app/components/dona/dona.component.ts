1199801135eea21171d6c17353b6f45b504897a0
Emmanuel Ponce <jesus.ponce.supernova@gmail.com>
2020-11-15 15:52:05 -0600

    dona component

---
+++ src/app/components/dona/dona.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MultiDataSet, Label, Color } from 'ng2-charts';
+
+@Component({
+  selector: 'app-dona',
+  templateUrl: './dona.component.html',
+  styleUrls: ['./dona.component.css']
+})
+export class DonaComponent{
+
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100]
+  ];
+  public colors:Color[] = [
+    {backgroundColor: ['#9E120E', '#FF5800', '#ffB414']}
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
